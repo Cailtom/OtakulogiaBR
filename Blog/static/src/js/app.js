cdd0ef29bf0fce4381492d892d2e07745e01c495
Cailtom <casiooliveyracosta@gmail.com>
2022-12-26 22:28:15 -0300

    feat: added webpack for bootstrap

---
+++ Blog/static/src/js/app.js
@@ -0,0 +1,3 @@
+import Offcanvas from '../../node_modules/bootstrap/js/dist/offcanvas'
+import '../../node_modules/bootstrap/scss/bootstrap.scss'
+
---
+++ Blog/static/webpack.config.js
@@ -0,0 +1,40 @@
+const modoDev = process.env.NODE_ENV !== 'production';
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+  mode: modoDev ? 'development' : 'production',
+  entry: './src/js/app.js',
+  output: {
+    filename: "bootstrap-min.js",
+    path: __dirname + "/dist",
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'bootstrap-min.css'
+    })
+  ],
+  module: {
+    rules: [{
+      test: /\.s?[ac]ss$/i,
+      use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+    }]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+        parallel: true
+      }),
+      new CssMinimizerPlugin(),
+    ]
+  }
+}
