e21478af63d435a30c69183dc9f17fd2aa669bf0
Cailtom <casiooliveyracosta@gmail.com>
2022-12-29 20:32:35 -0300

    feat: modifying the module import files

--- Blog/static/src/js/app.js
+++ Blog/static/src/js/app.js
@@ -1,3 +1,2 @@
-import Offcanvas from '../../node_modules/bootstrap/js/dist/offcanvas'
-import '../../node_modules/bootstrap/scss/bootstrap.scss'
-
+import "./bootstrap-modules";
+import "./bootstrap-sass";
